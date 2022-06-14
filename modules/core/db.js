d9ed3afaf22dee98ff041341d1f457bed29e1a34
matsqa <iliunovmat@gmail.com>
2022-06-13 20:17:43 -0700

    update url db

--- modules/core/db.js
+++ modules/core/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 module.exports = function dbConnect() {
-  mongoose.connect('mongodb://localhost/todos', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    'mongodb+srv://ichigo3d:Le@rning!984@cluster0.nb2vx7y.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function () {
